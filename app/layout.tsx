dba7f32d07c5c4834bf68ae3a544968572a4db34
DAVIN HENRY MONTEIRO <davinmonteiro@gmail.com>
2023-07-02 15:09:49 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './css/style.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter, Architects_Daughter } from 'next/font/google'
+
+import Header from '@/components/ui/header'
+import Banner from '@/components/banner'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap'
+})
+
+const architects_daughter = Architects_Daughter({
+  subsets: ['latin'],
+  variable: '--font-architects-daughter',
+  weight: '400',
+  display: 'swap'
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +30,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
+        <div className="flex flex-col min-h-screen overflow-hidden">
+          <Header />
+          {children}
+          <Banner />
+        </div>
+      </body>
     </html>
   )
 }
+ 
---
+++ components/features.tsx
@@ -0,0 +1,60 @@
+export default function Features() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">Insert any PDF/URL and watch the magic happen !!!</h2>
+           
+          </div>
+
+          {/* Items */}
+          <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
+
+            {/* 1st item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
+              </svg>
+              <h4 className="h4 mb-2">Rsearch and education</h4>
+              <p className="text-lg text-gray-400 text-center">PDF summarization in education and research streamlines the extraction of relevant information from research papers and educational resources. It accelerates literature review, enables efficient data analysis, and supports knowledge synthesis, enhancing the speed and effectiveness of academic pursuits and scholarly endeavors.</p>
+            </div>
+
+            {/* 2nd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <circle className="fill-current text-purple-600" cx="32" cy="32" r="32" />
+                <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
+              </svg>
+              <h4 className="h4 mb-2">Medical</h4>
+              <p className="text-lg text-gray-400 text-center">summarization of your diagnostic report provided you with valuable insights and helped you identify what was missing. The instant summarization can save time and assist in comprehending the key details, facilitating a more thorough understanding of your medical condition and potentially prompting further discussions with your healthcare provider.</p>
+            </div>
+
+            {/* 3rd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <g transform="translate(21 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
+                  <ellipse className="stroke-current text-purple-300" cx="11" cy="11" rx="5.5" ry="11" />
+                  <path className="stroke-current text-purple-100" d="M11 0v22M0 11h22" />
+                  <circle className="stroke-current text-purple-100" cx="11" cy="11" r="11" />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Judiciary</h4>
+              <p className="text-lg text-gray-400 text-center">For extracting key information from court cases,provide instant solutions to complex legal problems. It can assist in quickly identifying relevant details and facilitating a preliminary understanding of the case.</p>
+            </div>
+
+           
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
